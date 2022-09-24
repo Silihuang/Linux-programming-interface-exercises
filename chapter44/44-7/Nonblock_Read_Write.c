8bce3da55fdbd97afad811d4a661e03f09a611e3
Silihuang <75418873+Silihuang@users.noreply.github.com>
2022-09-24 13:43:39 +0800

    Add files via upload

---
+++ chapter44/44-7/Nonblock_Read_Write.c
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main( int argc, char *argv[] ) {
+    char buf[200] ;
+    int fd ;
+
+    mkfifo( "R_W", 0666 ) ;
+    fd = open("R_W", O_RDWR | O_NONBLOCK ) ; // O_NONBLOCK
+    
+    // write( fd, "R_W Success", strlen("R_W Success") + 1 ) ;
+    read( fd, buf, 200 ) ;
+
+    printf("Success\n" ) ;
+    close(fd) ;
+    unlink("R_W") ;
+    return 0 ;
+
+} // main
---
+++ chapter44/44-7/Nonblock_open.c
@@ -0,0 +1,24 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main( int argc, char *argv[] ) {
+
+    char buf[200] ;
+    int fd ;
+
+
+    mkfifo( "open", 0666 ) ;
+    fd = open("open", O_RDONLY | O_NONBLOCK ) ; // O_NONBLOCK
+    
+    printf("Open Success\n");
+    close(fd) ;
+    unlink("open") ;
+    return 0 ;
+
+} // main
